69ae6e74a208c2c5e518c7e5dc7c9b225fff0917
BrianCottrell <Brian_Cottrell@inbox.com>
2015-02-19 22:17:02 -0800

    Added results page.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var router      = express.Router();
 //Specify a traitify deck to use
 var deckId      = 'core';
 var assessmentId;
-//var listData    = '';
+var listData    = '';
 
 //Configure body body-parser
 app.use(bodyParser.urlencoded({extend:true}));
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, '/views')));
 mongoose.connect('mongodb://brian:mongo@ds039441.mongolab.com:39441/briansdatabase');
 
 //Specify routes
-router.route('/home')
+router.route('/')
 //Called when a user navigates to the home page
 .get(function(req, res){
     //Create a new traitify assessment
@@ -47,9 +47,14 @@ router.route('/home')
         // Use assessment here.
         assessmentId = assessment.id;
     });
-    res.status(200).render('home', {
-        assessmentId: assessmentId,
-//        listData: listData
+    Profile.find(function(err, data){
+        if(err){
+            console.log(err);
+        }
+        res.status(200).render('home', {
+            assessmentId: assessmentId,
+            listData: data,
+        });
     });
 })
 //Called when a user makes a post to the home page
@@ -68,24 +73,9 @@ router.route('/home')
         if(err){
             console.log(err);
         }
-        res.status(201).json({message: 'path seuccessfully created'});
     });
-//    res.redirect('/results');
 });
-//The results page is displayed when a user completes the assessment
-// router.route('/results')
-// //Called when a user completes the assessment and submits information
-// .get(function(req, res){
-//     listData = Profile.find(function(err, data){
-//         if(err){
-//             console.log(err);
-//         }
-//   //      res.status(200).render('results', {
-//     //        listData: data,
-//       //  });
-//     });
-// });
-
+//Add the router and start the server
 app.use(router);
 app.listen(port);
 console.log('Listening to port:', port);
